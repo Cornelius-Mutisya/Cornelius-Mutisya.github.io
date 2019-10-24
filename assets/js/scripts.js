afff316d2e197bf36b0a41ef6e990d1f4cea288d
Cornelius Mutisya <cornelius.mutisya@students.dkut.ac.ke>
2019-10-24 10:14:49 +0300

    Second

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,7 @@
 /*
 Theme Name: IAMX
-Author: Trendy Theme
-Author URL: trendytheme.net
+Author: Ahmed Faruk
+Author URL: farukahmed.com
 */
 
 /*
@@ -17,6 +17,7 @@ Author URL: trendytheme.net
     = Magnific Popup
     = Vidio auto play
     = Fit Vids
+    = Google Map
 
 */
 
@@ -200,24 +201,10 @@ jQuery(function ($) {
         removalDelay: 300, // Delay in milliseconds before popup is removed
         mainClass: 'mfp-with-zoom', // this class is for CSS animation below
         type:'image'
+
       });
 
     }());
-
-
-
-    (function () {
-        $('.popup-video').magnificPopup({
-            disableOn: 700,
-            type: 'iframe',
-            mainClass: 'mfp-with-zoom',
-            removalDelay: 300,
-            preloader: false,
-            fixedContentPos: false
-        });
-    }());
-
-
 
 
 
@@ -285,38 +272,75 @@ jQuery(function ($) {
 
     }());
 
-
-
     // -------------------------------------------------------------
-    // Contact Form
+    // Google Map
     // -------------------------------------------------------------
 
-    $('#contactForm').on('submit',function(e){
+    (function () {
+        var myLatlng = new google.maps.LatLng(41.372641, -74.687387);
 
-        e.preventDefault();
+            var styles = [
+                {
+                    featureType: "landscape",
+                    stylers: [
+                        { color: '#f7f7f7' }
+                    ]
+                },{
+                    featureType: "natural",
+                    stylers: [
+                        { hue: '#00ffe6' }
+                    ]
+                },{
+                    featureType: "road",
+                    stylers: [
+                        { hue: '#fff' },
+                        { saturation: -70 }
+                    ]
+                },{
+                    featureType: "building",
+                    elementType: "labels",
+                    stylers: [
+                        { hue: '' }
+                    ]
+                },{
+                    featureType: "poi", //points of interest
+                    stylers: [
+                        { hue: '' }
+                    ]
+                }
+            ];
 
-        var $action = $(this).prop('action');
-        var $data = $(this).serialize();
-        var $this = $(this);
-
-        $this.prevAll('.alert').remove();
-
-        $.post( $action, $data, function( data ) {
-
-            if( data.response=='error' ){
-
-                $this.before( '<div class="alert alert-danger">'+data.message+'</div>' );
+            var mapOptions = {
+                zoom: 15,
+                scrollwheel: false,
+                center: myLatlng,
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                disableDefaultUI: true,
+                styles: styles
             }
+            var map = new google.maps.Map(document.getElementById('mapCanvas'), mapOptions);
 
-            if( data.response=='success' ){
+            var marker = new google.maps.Marker({
+                position: myLatlng,
+                map: map,
+                animation: google.maps.Animation.DROP,
+                title: 'Hello World!'
+            });
 
-                $this.before( '<div class="alert alert-success">'+data.message+'</div>' );
-                $this.find('input, textarea').val('');
-            }
+            var contentString = '' +
+                    '' +
+                    '';
 
-        }, "json");
+            var infowindow = new google.maps.InfoWindow({
+                content: contentString
+            });
 
-    });
+            google.maps.event.addListener(marker, 'click', function () {
+                infowindow.open(map, marker);
+            });
+
+    }());
+
 
 });
 
